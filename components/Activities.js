56174a9763443288c16362ced1e2e4bb3b536caa
romane 29git <rcotecoliss@ensc.fr>
2023-07-17 16:10:04 +0200

    checkbox liste acti

--- components/Activities.js
+++ components/Activities.js
@@ -2,15 +2,21 @@ import { Text, View, FlatList, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import activitiesApi from "../api/activitiesApi";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import Checkbox from "expo-checkbox";
 
 const Activities = () => {
   const [activities, setActivities] = useState([]);
+  const [isChecked, setChecked] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       const fetchedActivities = await activitiesApi.fetchActivities();
+      const updatedActivities = fetchedActivities.map((activity) => ({
+        ...activity,
+        isChecked: false, // Ajout de la propriété isChecked avec la valeur par défaut false
+      }));
       console.log(fetchedActivities);
-      setActivities(fetchedActivities);
+      setActivities(updatedActivities);
     }
 
     fetchData();
@@ -23,33 +29,48 @@ const Activities = () => {
       </View>
     );
   };
-  const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Text style={styles.name}>{item.name}</Text>
-      <View style={styles.titleContainer}>
-        <BubbleIcon
-          iconName={
-            item.type_event === "call"
-              ? "phone"
-              : item.type_event === "meeting"
-              ? "users"
-              : item.type_event === "mail"
-              ? "envelope"
-              : "tasks"
-          }
+  const renderItem = ({ item }) => {
+    const handleCheckboxChange = (isChecked) => {
+      const index = activities.findIndex((activity) => activity.id === item.id);
+      if (index !== -1) {
+        const updatedActivities = [...activities];
+        updatedActivities[index].isChecked = isChecked;
+        setActivities(updatedActivities);
+      }
+    };
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Checkbox
+          style={styles.checkbox}
+          value={item.isChecked}
+          onValueChange={handleCheckboxChange}
         />
-        <Text style={styles.city}> {item.type_event}</Text>
+        <View style={styles.titleContainer}>
+          <BubbleIcon
+            iconName={
+              item.type_event === "call"
+                ? "phone"
+                : item.type_event === "meeting"
+                ? "users"
+                : item.type_event === "mail"
+                ? "envelope"
+                : "tasks"
+            }
+          />
+          <Text style={styles.city}> {item.type_event}</Text>
+        </View>
+        <Text style={styles.info}>Durée : {item.duration} minutes</Text>
+        <Text style={styles.info}>Date : {item.date_start}</Text>
+        <Text style={styles.info}>
+          {item.companyName ? `Organisation : ${item.companyName}` : null}
+        </Text>
+        <Text style={styles.info}>
+          {item.contactName ? `Contact : ${item.contactName}` : null}
+        </Text>
       </View>
-      <Text style={styles.info}>Durée : {item.duration} minutes</Text>
-      <Text style={styles.info}>Date : {item.date_start}</Text>
-      <Text style={styles.info}>
-        {item.companyName ? `Organisation : ${item.companyName}` : null}
-      </Text>
-      <Text style={styles.info}>
-        {item.contactName ? `Contact : ${item.contactName}` : null}
-      </Text>
-    </View>
-  );
+    );
+  };
 
   return (
     <View style={styles.container}>
